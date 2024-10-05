07d4bd19a1eb44a225416b0eb983938dd6b0cc7b
Keerthana Ravikumar <kravik10@asu.edu>
2024-10-05 13:03:35 -0700

    feat: Implemented UI for deleting a User Story

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/widgets/UserStoryWidget.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/widgets/UserStoryWidget.java
@@ -3,13 +3,14 @@ package com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets;
 import com.groupesan.project.java.scrumsimulator.mainpackage.impl.UserStory;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels.EditUserStoryForm;
 import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import java.awt.*;
 
 public class UserStoryWidget extends JPanel implements BaseComponent {
 
@@ -17,6 +18,7 @@ public class UserStoryWidget extends JPanel implements BaseComponent {
     JLabel points;
     JLabel name;
     JLabel desc;
+    JLabel deleteIcon;
 
     // TODO: This is a non transient field and this class is supposed to be serializable. this needs
     // to be dealt with before this object can be serialized
@@ -47,36 +49,70 @@ public class UserStoryWidget extends JPanel implements BaseComponent {
     }
 
     public void init() {
-        removeAll();
+        removeAll();        
 
         id = new JLabel(userStory.getId().toString());
         id.addMouseListener(openEditDialog);
+
         points = new JLabel(Double.toString(userStory.getPointValue()));
         points.addMouseListener(openEditDialog);
+
         name = new JLabel(userStory.getName());
         name.addMouseListener(openEditDialog);
+
         desc = new JLabel(userStory.getDescription());
         desc.addMouseListener(openEditDialog);
+
+        deleteIcon = new JLabel(
+            new ImageIcon(
+                new ImageIcon(
+                    System.getProperty("user.dir") + "/assets/delete-icon.png"
+                ).getImage().getScaledInstance(14, 14, Image.SCALE_SMOOTH)
+            )
+        );
 
         GridBagLayout myGridBagLayout = new GridBagLayout();
 
         setLayout(myGridBagLayout);
 
         add(
-                id,
-                new CustomConstraints(
-                        0, 0, GridBagConstraints.WEST, 0.1, 0.0, GridBagConstraints.HORIZONTAL));
+            id,
+            new CustomConstraints(
+                0, 0, GridBagConstraints.WEST, 
+                0.1, 0.0, GridBagConstraints.HORIZONTAL
+            )
+        );
+
         add(
-                points,
-                new CustomConstraints(
-                        1, 0, GridBagConstraints.WEST, 0.1, 0.0, GridBagConstraints.HORIZONTAL));
+            points,
+            new CustomConstraints(
+                1, 0, GridBagConstraints.WEST, 
+                0.1, 0.0, GridBagConstraints.HORIZONTAL
+            )
+        );
+
         add(
-                name,
-                new CustomConstraints(
-                        2, 0, GridBagConstraints.WEST, 0.2, 0.0, GridBagConstraints.HORIZONTAL));
+            name,
+            new CustomConstraints(
+                2, 0, GridBagConstraints.WEST, 
+                0.2, 0.0, GridBagConstraints.HORIZONTAL
+            )
+        );
+
         add(
-                desc,
-                new CustomConstraints(
-                        3, 0, GridBagConstraints.WEST, 0.7, 0.0, GridBagConstraints.HORIZONTAL));
+            desc,
+            new CustomConstraints(
+                3, 0, GridBagConstraints.WEST, 
+                0.7, 0.0, GridBagConstraints.HORIZONTAL
+            )
+        );
+
+        add(
+            deleteIcon,
+            new CustomConstraints(
+                5, 0, GridBagConstraints.WEST, 
+                0.7, 0.0, GridBagConstraints.HORIZONTAL
+            )
+        );
     }
 }
