77cad44615251394ca80250ab4339f90338c313b
Rahul3225 <rravin15@asu.edu>
2024-11-01 19:19:44 -0700

    Task 113 of User story 93 Backend Implementation

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/widgets/PossibleBlockersListPane.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/widgets/PossibleBlockersListPane.java
@@ -1,27 +1,19 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets;
 
-import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.PossibleBlockerWidget;
 import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
 import com.groupesan.project.java.scrumsimulator.mainpackage.impl.PossibleBlocker;
 import com.groupesan.project.java.scrumsimulator.mainpackage.impl.PossibleBlockersStore;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.Solutions;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class PossibleBlockersListPane extends JFrame implements BaseComponent {
     private JPanel _subPanel;
@@ -34,45 +26,15 @@ public class PossibleBlockersListPane extends JFrame implements BaseComponent {
     public void init() {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle("Possible Blockers");
-        setSize(400, 300);
+        setPreferredSize(new Dimension(500, 400)); 
+        setResizable(false); 
 
         JPanel myJPanel = new JPanel(new GridBagLayout());
         myJPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        // Dummy data
-        PossibleBlockersStore.getInstance().addNewBlocker(
-            new PossibleBlocker("Test 1", "It's a dummy desc for the possible blocker")
-        );
-        PossibleBlockersStore.getInstance().addNewBlocker(
-            new PossibleBlocker("Test 2", "It's a dummy desc for the possible blocker")
-        );
-        PossibleBlockersStore.getInstance().addNewBlocker(
-            new PossibleBlocker("Test 3", "It's a dummy desc for the possible blocker")
-        );
-        PossibleBlockersStore.getInstance().addNewBlocker(
-            new PossibleBlocker("Test 4", "It's a dummy desc for the possible blocker")
-        );
-        
-
         _subPanel = new JPanel(new GridBagLayout());
-
-        List<PossibleBlocker> blockers = PossibleBlockersStore.getInstance().getListOfPossibleBlockers();
-        for (int i = 0; i < PossibleBlockersStore.getInstance().size(); i++) {
-
-            PossibleBlocker pbw = blockers.get(i);
-            JButton solutionListButton = new JButton(pbw.print());
-            solutionListButton.addActionListener(e -> {
-                Solutions solutions = new Solutions(pbw.getId());
-                solutions.setVisible(true);
-
-            });
-            _subPanel.add(
-                solutionListButton,
-                new CustomConstraints(
-                    0, i, GridBagConstraints.WEST, 1.0, 0.1, GridBagConstraints.HORIZONTAL
-                )
-            );
-        }
+        _subPanel.setPreferredSize(new Dimension(300, 5));
+        refreshBlockersList(); 
 
         myJPanel.add(
             new JScrollPane(_subPanel),
@@ -82,15 +44,10 @@ public class PossibleBlockersListPane extends JFrame implements BaseComponent {
         );
 
         JButton newBlockerButton = new JButton("New Blocker Story");
-        newBlockerButton.addActionListener(
-            new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    NewBlockerListPane formb = new NewBlockerListPane();
-                    formb.setVisible(true);
-                }
-            }
-        );
+        newBlockerButton.addActionListener(e -> {
+            NewBlockerListPane formb = new NewBlockerListPane(this); 
+            formb.setVisible(true);
+        });
 
         myJPanel.add(
             newBlockerButton,
@@ -98,17 +55,12 @@ public class PossibleBlockersListPane extends JFrame implements BaseComponent {
                 0, 1, GridBagConstraints.WEST, 1.0, 0.1, GridBagConstraints.HORIZONTAL
             )
         );
-        
+
         JButton fineTuneButton = new JButton("Fine Tune Probabilities");
-        fineTuneButton.addActionListener(
-            new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    FineTuneListPane form = new FineTuneListPane();
-                    form.setVisible(true);
-                }
-            }
-        );
+        fineTuneButton.addActionListener(e -> {
+            FineTuneListPane form = new FineTuneListPane();
+            form.setVisible(true);
+        });
 
         myJPanel.add(
             fineTuneButton,
@@ -116,10 +68,33 @@ public class PossibleBlockersListPane extends JFrame implements BaseComponent {
                 0, 2, GridBagConstraints.WEST, 1.0, 0.1, GridBagConstraints.HORIZONTAL
             )
         );
-        
+
         setContentPane(myJPanel);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    public void refreshBlockersList() {
+        _subPanel.removeAll(); 
+        List<PossibleBlocker> blockers = PossibleBlockersStore.getInstance().getListOfPossibleBlockers();
+    
+        for (int i = 0; i < blockers.size(); i++) {
+            PossibleBlocker pbw = blockers.get(i);
+            JButton solutionListButton = new JButton(pbw.print());
+            solutionListButton.addActionListener(e -> {
+                Solutions solutions = new Solutions(pbw.getId());
+                solutions.setVisible(true);
+            });
+            _subPanel.add(
+                solutionListButton,
+                new CustomConstraints(
+                    0, i, GridBagConstraints.WEST, 1.0, 0.1, GridBagConstraints.HORIZONTAL
+                )
+            );
+        }
+    
+        _subPanel.revalidate();
+        _subPanel.repaint();
     }
 }
